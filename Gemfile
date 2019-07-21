4f800454525570a1d779b70599d787c07089f8b3
bilbonlehobbit <bilbonlehobbit22@gmail.com>
2019-07-21 05:24:57 +0200

    fin des pages statiques

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  
+  gem 'capybara'
 end
 
 group :development do
@@ -55,8 +55,12 @@ end
 group :test do
   gem 'rspec'
   gem 'webrat'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 #gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'protected_attributes_continued'
+gem 'autotest-notification'
+gem 'autotest'
+gem 'autotest-rails-pure'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+     render_views   
+before(:each) do
 
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
-  end
+@response_httpstatus = expect(response).to have_http_status(:success)
 
-  describe "GET #contact" do
-    it "returns http success" do
-      get :contact
-      expect(response).to have_http_status(:success)
-    end
-  end
+
+@base_title = "Simple App du Tutoriel Ruby on Rails | "
+#@response_title = expect(response.body).to have_title("Simple App du Tutoriel Ruby on Rails | ")
 
 end
+ 			 describe "GET #home" do
+	before(:each) do 
+		get :home 
+		  
+	end
+		it "returns http success" do
+                  @response_httpstatus 
+  end
+ 	  	it "doit avoir le bon titre" do 
+		 
+		expect(response.body).to have_title(@base_title + "Accueil")
+  end
+
+
+end
+
+ 			 describe "GET #contact" do
+	before(:each) do 
+		get :contact
+		  
+	end
+		it "returns http success" do
+                 @response_httpstatus 
+  end
+ 	  	it "doit avoir le bon titre" do  
+		expect(response.body).to have_title(@base_title + "Contact")
+  end
+
+
+ 			 describe "GET #about" do
+	before(:each) do 
+		get :about
+		  
+	end
+		it "returns http success" do
+                 @response_httpstatus 
+  end
+ 	  	it "doit avoir le bon titre" do  
+		expect(response.body).to have_title(@base_title + "A Propos")
+       end
+    end
+  end
+end
+
